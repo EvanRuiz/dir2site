7310dcd8bcbbc2eae186d659d783f9ec9740a3cd
Evan Ruiz <evanruiz@pm.me>
2025-11-25 13:25:46 -0500

    build script (windows)
    - PowerShell Script that runs on Windows
    - Run bat for convenience included in windows zip build (to be replaced when/if official installer)

---
+++ OpenSeadragonOverlayeditor.bat
@@ -0,0 +1,4 @@
+REM TODO: When/if there is an official installer, this convenience bat won't be needed
+@echo off
+cd bin
+start "OpenSeadragon Overlay Editor" OpenSeadragonOverlayEditor.exe
---
+++ build-win-x64.ps1
@@ -0,0 +1,16 @@
+dotnet publish -r win-x64 -p:Platform=x64 -p:Configuration=Release --self-contained
+
+New-Item -Path "builds" -ItemType Directory -Force
+
+# For windows build, place all build files under /bin in zip
+Remove-Item -Force -Recurse -Path "bin/x64/Release/net9.0/win-x64/bin"
+Move-Item -Force -Path "bin/x64/Release/net9.0/win-x64/publish" -Destination "bin/x64/Release/net9.0/win-x64/bin"
+
+Compress-Archive -Force -Path "bin/x64/Release/net9.0/win-x64/bin" -DestinationPath "builds/OpenSeadragonOverlayEditor-win-x64.zip"
+
+# Convenience bat for quickly running after unzip with build files encapsulated under the bin folder
+# TODO: When/if official installer, this won't be necessary
+Compress-Archive -Update -Path "OpenSeadragonOverlayEditor.bat" -DestinationPath "builds/OpenSeadragonOverlayEditor-win-x64.zip"
+
+
+
